9075460161d0c92cd4a348f9454a1997e4513b4b
fitakabisil <112487979+fitakabisil@users.noreply.github.com>
2022-09-26 15:43:10 -0400

    Delete work1.py

--- work1.py
+++
@@ -1,4 +0,0 @@
-chaine = input("ecrivez votre phrase")  #chaine est la variable qui va representer le 'string' original
-liste_de_mots = chaine.split()  #le code split() va transformer la chaine(string) en une liste.
-nombre_de_mots = len(liste_de_mots)  #de ce que j'ai compris, len() va transformer la liste en 'string'
-print(nombre_de_mots)  #ceci est la sortie (output)
